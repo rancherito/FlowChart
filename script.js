3c9c6e020136316f9e371eddca15d585de77b3ae
CoffeeDuck <davidlive0159@gmail.com>
2023-04-05 02:51:53 -0500

    up

--- script.js
+++ script.js
@@ -1,181 +1,230 @@
 
 const { createApp } = Vue
-/*
-const cajaA = document.getElementById('cajaA');
-const cajaB = document.getElementById('cajaB');
-
-let arrastrando = false;
-let posicionInicialX = 0;
-let posicionInicialY = 0;
-let desplazamientoX = 0;
-let desplazamientoY = 0;
-
-cajaB.addEventListener('mousedown', (evento) => {
-  arrastrando = true;
-  posicionInicialX = evento.clientX - desplazamientoX;
-  posicionInicialY = evento.clientY - desplazamientoY;
-});
-
-cajaB.addEventListener('mousemove', (evento) => {
-  if (!arrastrando) return;
-
-  desplazamientoX = evento.clientX - posicionInicialX;
-  desplazamientoY = evento.clientY - posicionInicialY;
-
-  const maxX = cajaB.clientWidth - cajaA.clientWidth;
-  const maxY = cajaB.clientHeight - cajaA.clientHeight;
-
-  desplazamientoX = Math.min(Math.max(desplazamientoX, maxX), 0);
-  desplazamientoY = Math.min(Math.max(desplazamientoY, maxY), 0);
-
-  cajaA.style.transform = `translate(${desplazamientoX}px, ${desplazamientoY}px)`;
-});
-
-cajaB.addEventListener('mouseup', () => {
-  arrastrando = false;
-});
-
-cajaB.addEventListener('mouseleave', () => {
-  arrastrando = false;
-});
-
-function crearRectangulo(x, y, width, height, fill) {
-  const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-  rect.setAttribute("x", x);
-  rect.setAttribute("y", y);
-  rect.setAttribute("width", width);
-  rect.setAttribute("height", height);
-  rect.setAttribute("rx", 10);
-  rect.setAttribute("ry", 10);
-  rect.setAttribute("fill", fill);
-  return rect;
-}
-
-const rectangulo1 = crearRectangulo(50, 50, 100, 50, "blue");
-const rectangulo2 = crearRectangulo(250, 250, 100, 50, "green");
-const rectangulo3 = crearRectangulo(500, 250, 100, 50, "gray");
-
-cajaA.appendChild(rectangulo1);
-cajaA.appendChild(rectangulo2);
-cajaA.appendChild(rectangulo3);*/
 
 const boxChart = {
-  props: {
-    posX: {
-      type: Number,
-      required: true,
-      default: 0
+    props: {
+        parents: {
+            type: Array,
+            default: []
+        },
+        modelValue: {
+            type: Object,
+            default: {
+                id: Math.random(),
+                x: 0,
+                y: 0,
+                width: 100,
+                height: 50,
+                color: 'blue'
+            }
+        },
+        globalPosition: {
+            type: Object,
+            default: {
+                x: 0,
+                y: 0
+            }
+        },
     },
-    posY: {
-      type: Number,
-      required: true,
-      default: 0
+
+    emits: ['update:modelValue'],
+    data() {
+        return {
+            width: this.modelValue?.width ?? 100,
+            height: this.modelValue?.height ?? 50,
+            id: this.modelValue?.id ?? Math.random(),
+            childXMenorCHildYMenor: 0,
+            childXMenorCHildYMayor: 1,
+            childXMayorCHildYMenor: 2,
+            childXMayorCHildYMayor: 3,
+            isMove: false,
+        }
     },
-    width: {
-      type: Number,
-      default: 100
+    watch: {
+        globalPosition: {
+            handler(val) {
+                if (this.isMove) {
+                    this.modelValue.x = val.x;
+                    this.modelValue.y = val.y;
+                    this.$emit('update:modelValue', JSON.parse(JSON.stringify(this.modelValue)));
+                }
+            },
+            deep: true
+        }
     },
-    height: {
-      type: Number,
-      default: 50
+    computed: {
+        posX() {
+            return this.modelValue?.x ?? 0;
+        },
+        posY() {
+            return this.modelValue?.y ?? 0;
+        },
+        styleTranslate() {
+            let x = this.posX - (this.width / 2);
+            let y = this.posY - (this.height / 2);
+            return `translate(${x}, ${y})`;
+        },
+        lines() {
+            const gap = 16;
+            const ele = this.parents.map(parent => {
+                const parentHeight = (parent?.height ?? 50) / 2;
+                const parentWidth = parent?.width ?? 100;
+                const startX = 0;
+                const startY = this.height / 2;
+                const endX = parent.x - this.posX + parentWidth;
+                const endY = parent.y - this.posY + parentHeight;
+
+                const startPath = `M${startX} ${startY} L ${startX - gap} ${startY}`;
+                const endPath = `L ${endX + gap} ${endY} L ${endX} ${endY}`;
+
+                const childXIsMenor = (this.posX + (this.width / 2) < parent.x + (parentWidth / 2)) + 0;
+                const childYIsMenor = (this.posY + (this.height / 2) < parent.y + (parentHeight / 2)) + 0;
+
+                const state = parseInt(`${childXIsMenor}${childYIsMenor}`, 2);
+
+                let ret = {
+                    translate: `translate(${startX - 5}, ${startY - 5}) rotate(45, 5, 3.5)`
+                };
+
+                console.log(state);
+                switch (state) {
+                    case this.childXMenorCHildYMayor:
+
+                        ret.d = `${startPath} L ${startX - gap} ${this.height + gap} L ${endX + gap} ${this.height + gap} ${endPath}`
+                        ret.stroke = 'red'
+
+                        break;
+                    case this.childXMenorCHildYMenor:
+
+                        ret.d = `${startPath} L ${startX - gap} ${0 - gap} L ${endX + gap} ${0 - gap} ${endPath}`
+                        ret.stroke = 'PURPLE'
+
+                        break;
+                    case this.childXMayorCHildYMenor:
+
+                        ret.d = `${startPath} L ${startX - gap} ${0 - gap} L ${endX + gap} ${0 - gap} ${endPath}`
+                        ret.stroke = 'blue'
+
+                        break;
+                    default:
+
+                        ret.d = `${startPath} L ${startX - gap} ${this.height + gap} L ${endX + gap} ${this.height + gap} ${endPath}`
+                        ret.stroke = 'green'
+
+                        break;
+                }
+                return ret;
+
+            });
+            return ele;
+        }
     },
-    color: {
-      type: String,
-      required: true,
-      default: 'blue'
-    }
-  },
-  computed: {
-    styleTranslate() {
-      let x = this.posX - (this.width / 2);
-      let y = this.posY - (this.height / 2);
-      return `translate(${x}, ${y})`;
-    }
-  },
-  template: `
+    mounted() {
+    },
+    methods: {
+        onDown() {
+            console.log('onDown');
+            this.isMove = true
+        },
+        onUp() {
+            console.log('onUp');
+            this.isMove = false
+        }
+    },
+    template: `
     <g :transform="styleTranslate">
-      <rect class="box-chart-box" x="0" y="0" :width="width" :height="height" fill="white" rx="5" ry="5" style="stroke: black; fill: white"/>
-      <rect :y="(height - 10) / 2" :x="(0 - 5)" width="10" height="10" fill="gray" rx="2" ry="2"/>
-      <rect :y="(height - 10) / 2" :x="(width - 5)" width="10" height="10" fill="gray" rx="2" ry="2"/>
-      <text class="box-chart-text" :x="width / 2" :y="height / 2" text-anchor="middle" alignment-baseline="central">LOREM</text>
+        <rect @mousedown="onDown" @mouseup="onUp" @mouse class="box-chart-box" x="0" y="0" :width="width" :height="height" fill="white" rx="5" ry="5" style="stroke: black; fill: white"/>
+        <text class="box-chart-text box-chart-noevent" :x="width / 2" :y="height / 2" text-anchor="middle" alignment-baseline="central">{{'BOX ' + id}} {{isMove}}</text>
+        <template v-for="line in lines">
+            <path class="box-chart-line" :d="line.d" :stroke="line.stroke"/>
+            <path :transform="line.translate" d="M3 0 L8 0 A2 2 0 0 1 10 2 L10 7 A2 3 0 0 1 7.5 7.5 L2.5 2.5 A3 2 0 0 1 3 0" fill="GRAY" />            
+        </template>
+        <rect :y="(height - 10) / 2" :x="(width - 5)" width="10" height="10" fill="gray" rx="2" ry="2"/>
     </g>
   `
 }
 
 const svgChart = {
-  props: {
-    boxs: []
-  },
-  data() {
-    return {
-      message: 'Hello Vue! child',
-      cajaA: null,
-      cajaB: null,
-      arrastrando: false,
-      posicionInicialX: 0,
-      posicionInicialY: 0,
-      desplazamientoX: 0,
-      desplazamientoY: 0,
-      transform: 'translate(0px, 0px)'
-    }
-  },
-  mounted() {
-
-    this.cajaA = this.$refs.cajaA;
-    this.cajaB = this.$refs.cajaB;
-  },
-  methods: {
-    mouseDownEvent(event) {
-      this.arrastrando = true;
-      this.posicionInicialX = event.clientX - this.desplazamientoX;
-      this.posicionInicialY = event.clientY - this.desplazamientoY;
+    props: {
+        boxs: []
     },
-    mouseMoveEvent(event) {
-      if (!this.arrastrando) return;
-
-      this.desplazamientoX = event.clientX - this.posicionInicialX;
-      this.desplazamientoY = event.clientY - this.posicionInicialY;
-
-      const maxX = this.cajaB.clientWidth - this.cajaA.clientWidth;
-      const maxY = this.cajaB.clientHeight - this.cajaA.clientHeight;
-
-      this.desplazamientoX = Math.min(Math.max(this.desplazamientoX, maxX), 0);
-      this.desplazamientoY = Math.min(Math.max(this.desplazamientoY, maxY), 0);
-
-      this.transform = `translate(${this.desplazamientoX}px, ${this.desplazamientoY}px)`;
+    data() {
+        return {
+            message: 'Hello Vue! child',
+            cajaA: null,
+            cajaB: null,
+            arrastrando: false,
+            posicionInicialX: 0,
+            posicionInicialY: 0,
+            desplazamientoX: 0,
+            desplazamientoY: 0,
+            transform: 'translate(0px, 0px)',
+            globalPosition: {
+                x: 0,
+                y: 0
+            }
+        }
     },
-    dragOff() {
-      this.arrastrando = false;
-    }
-  },
-  template: `
+    mounted() {
+
+        this.cajaA = this.$refs.cajaA;
+        this.cajaB = this.$refs.cajaB;
+    },
+    methods: {
+        mouseDownEvent(event) {
+            if (event.target.id != 'cajaA') return
+            this.arrastrando = true;
+            this.posicionInicialX = event.clientX - this.desplazamientoX;
+            this.posicionInicialY = event.clientY - this.desplazamientoY;
+        },
+        mouseMoveEvent(event) {
+            this.globalPosition.x = event.offsetX;
+            this.globalPosition.y = event.offsetY;
+            if (!this.arrastrando) return;
+
+            this.desplazamientoX = event.clientX - this.posicionInicialX;
+            this.desplazamientoY = event.clientY - this.posicionInicialY;
+
+            const maxX = this.cajaB.clientWidth - this.cajaA.clientWidth;
+            const maxY = this.cajaB.clientHeight - this.cajaA.clientHeight;
+
+            this.desplazamientoX = Math.min(Math.max(this.desplazamientoX, maxX), 0);
+            this.desplazamientoY = Math.min(Math.max(this.desplazamientoY, maxY), 0);
+
+            this.transform = `translate(${this.desplazamientoX}px, ${this.desplazamientoY}px)`;
+        },
+        dragOff() {
+            this.arrastrando = false;
+        }
+    },
+    template: `
  <div>
     {{ desplazamientoX }}, {{ desplazamientoY }}
     <div id="cajaB" @mousedown="mouseDownEvent" ref="cajaB">
-      <svg @mouseup="dragOff" :style="{transform: transform}" @mouseleave="dragOff" id="cajaA" width="1000" height="1000" @mousemove="mouseMoveEvent" ref="cajaA">
-        <box-chart v-for="rect in boxs" :posX="rect.x" :posY="rect.y" />
-        <path d="M10 10 L70 10 A30 30 0 0 1 100 40 L100 120 A20 20 0 0 0 120 140 L140 140" stroke="black" fill="none" />
-        <path d="M120 50 L190 50 V200" stroke="black" fill="none" />  
-      </svg>
+        <svg @mouseup="dragOff" :style="{transform: transform}" @mouseleave="dragOff" id="cajaA" width="2000" height="2000" @mousemove="mouseMoveEvent" ref="cajaA">
+            <box-chart v-for="rect in boxs" :globalPosition="globalPosition" v-model="rect" :parents="boxs.filter(x => Array.isArray(rect.parents) && rect.parents.includes(x.id))" />
+            <path d="M10 10 L70 10 A30 30 0 0 1 100 40 L100 120 A20 20 0 0 0 120 140 L140 140" stroke="black" fill="none" />
+            <path transform="translate(10,10) rotate(90, 5, 3.5)" d="M3 0 L8 0 A2 2 0 0 1 10 2 L10 7 A2 3 0 0 1 7.5 7.5 L2.5 2.5 A3 2 0 0 1 3 0" fill="red" />
+            
+
+        </svg>
     </div>
+    <ul><li v-for="rect in boxs">{{rect.x}}, {{rect.y}}</li></ul>
  </div>
   `
 }
 
 
 var app = createApp({
-  data() {
-    return {
-      message: 'Hello Vue!',
-      boxs: [
-        { x: 80, y: 700},
-        { x: 250, y: 250},
-        { x: 400, y: 250},
-        { x: 100, y: 150},
-
-      ]
+    data() {
+        return {
+            message: 'Hello Vue!',
+            boxs: [
+                { x: 700, y: 180, id: 1 },
+                { x: 800, y: 400, id: 3, parents: [1] },
+            ]
+        }
     }
-  }
 })
 app.component('svg-chart', svgChart);
 app.component('box-chart', boxChart);
