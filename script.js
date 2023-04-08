4ce0e3cce6f6f7a7f1a2f2ab0116dd128c7701b9
CoffeeDuck <davidlive0159@gmail.com>
2023-04-08 02:50:14 -0500

    up

--- script.js
+++ script.js
@@ -85,7 +85,6 @@ const boxChart = {
     <g :transform="styleTranslate">
         <rect @mousedown="onDown" @mouseup="onUp" @mouse class="flowchart-box" x="0" y="0" :width="width" :height="height" fill="white" rx="5" ry="5" style="stroke: black; fill: white"/>
         <text class="flowchart-text flowchart-noevent" :x="width / 2" :y="height / 2" text-anchor="middle" alignment-baseline="central">{{'BOX ' + id}}</text>
-        <rect :y="(height - 10) / 2" :x="(width - 5)" width="10" height="10" fill="gray" rx="2" ry="2"/>
     </g>
   `
 }
@@ -121,10 +120,10 @@ const lineChart = {
             width: this.modelValue?.width ?? 100,
             height: this.modelValue?.height ?? 50,
             id: this.modelValue?.id ?? Math.random(),
-            childXMenorCHildYMenor: 0,
-            childXMenorCHildYMayor: 1,
-            childXMayorCHildYMenor: 2,
-            childXMayorCHildYMayor: 3,
+            left: 0,
+            top: 1,
+            rigth: 2,
+            bottom: 3,
             isMove: false,
             colors: {
                 purple: '#673AB7',
@@ -139,7 +138,21 @@ const lineChart = {
         }
     },
     mounted() {
-        console.log(this.id)
+
+    },
+    methods: {
+        AnglePosition(Ax, Ay, Bx, By) {
+            const dx = Bx - Ax;
+            const dy = By - Ay;
+            const anguloRad = Math.atan2(dy, dx);
+            let anguloDeg = anguloRad * (180 / Math.PI);
+
+            anguloDeg = anguloDeg < 0 ? anguloDeg + 360 : anguloDeg;
+            if (anguloDeg >= 315 || anguloDeg < 45) return 2;
+            if (anguloDeg >= 45 && anguloDeg < 135) return 3;
+            if (anguloDeg >= 135 && anguloDeg < 225) return 0;
+            return 1;
+        }
     },
     computed: {
         posX() {
@@ -165,90 +178,90 @@ const lineChart = {
                 const childHeight = this.height;
                 const childWidth = this.width;
 
-                const endX = this.posX - (childWidth / 2);
+                const endX = this.posX;
                 const endY = this.posY;
-                const startX = parent.x + (parentWidth / 2);
+                const startX = parent.x;
                 const startY = parent.y;
 
-                const lineStartA = [[startX, startY], [startX + gap, startY]];
-                const lineEndA = [[endX - gap, endY], [endX, endY]];
+                const lineStartA = [[startX + (parentWidth / 2), startY], [startX + (parentWidth / 2) + gap, startY]];
+                const lineEndA = [[endX - (childWidth / 2) - gap, endY], [endX - (childWidth / 2), endY]];
 
-                const lineStartB = [[startX, startY], [startX + gap, startY]];
-                const lineEndB = [[endX - gap, endY], [endX, endY]];
+                const lineStartB = [[startX, startY + (parentHeight / 2)], [startX, startY + (parentHeight / 2) + gap]];
+                const lineEndB = [[endX, endY - (childHeight / 2) - gap], [endX, endY - (childHeight / 2)]];
 
                 const childXIsMenor = (this.posX < parent.x) + 0;
                 const childYIsMenor = (this.posY < parent.y) + 0;
 
                 const state = parseInt(`${childXIsMenor}${childYIsMenor}`, 2);
 
+                const angle = this.AnglePosition(parent.x, parent.y, this.posX, this.posY);
                 let lineDirection = {
-                    translate: `translate(${endX - 12}, ${endY - 5}) rotate(45, 5, 3.5)`
+                    translate: `translate(${endX - (childWidth / 2) - 12}, ${endY - 5}) rotate(45, 5, 3.5)`
                 };
+                if (angle == this.bottom || angle == this.left) {
+                    lineDirection.translate = `translate(${endX - 5}, ${endY - (childHeight / 2) - 10}) rotate(135, 5, 3.5)`
+                }
 
-                switch (state) {
-                    case this.childXMenorCHildYMenor:
-
-                        if (parent.x + (parentWidth / 2) > endX - (gap * 2)) {
+                switch (angle) {
+                    case this.left:
+                        lineDirection.stroke = this.colors.indigo
+                        isEndA = false;
+                        
+                        if (endY - (childHeight / 2) < startY - (parentHeight / 2)) {
+                            points.push([startX + (parentWidth / 2) + gap, endY - (childHeight / 2) - gap]);
+                        }
+                        else{
+                                
+                            
                             isStartA = false;
-                            points.push([startX + gap, startY + (parentHeight / 2) + gap]);
-                            points.push([endX - gap, startY + (parentHeight / 2) + gap]);
+                            points.push([startX-(parentWidth / 2) - gap, startY + (parentHeight / 2) + gap]);
+                            
+                            if (endY - (childHeight / 2) - gap * 2 < startY + (parentHeight / 2)) {
+                                points.push([startX -(parentWidth / 2) - gap, endY - (childHeight / 2) - gap]);
+                            }
+                            else {
+                                points.push([endX,startY + (parentHeight / 2) + gap]);
+                            }
+                        }
 
-                            lineDirection.stroke = 'black'
+                        break;
+                    case this.top:
+                        lineDirection.stroke = this.colors.teal
+                        if (endX - (childWidth / 2) - 2 * gap > startX + (parentWidth / 2)) {
+                            points.push([startX + (parentWidth / 2) + gap, endY]);
                         }
                         else {
-                            points.push([startX + gap, endY]);
-                            lineDirection.stroke = this.colors.indigo
-                        }
-
-                        break;
-                    case this.childXMenorCHildYMayor:
-
-                        if (parent.x + (parentWidth / 2) > endX - (gap * 2)) {
-                            points.push([startX + gap, startY - (parentHeight / 2) - gap]);
-                            points.push([endX - gap, startY - (parentHeight / 2) - gap]);
-                            lineDirection.stroke = this.colors.blue
-                        } else {
-                            points.push([startX + gap, endY]);
-                            lineDirection.stroke = this.colors.teal
-                        }
-
-                        break;
-                    case this.childXMayorCHildYMenor:
-                        if (startY + ((parentHeight + childHeight) / 2) + 2 * gap > endY) {
-                            points.push([startX + gap, endY + (childHeight / 2) + gap]);
-                            points.push([endX - gap, endY + (childHeight / 2) + gap]);
-                            lineDirection.stroke = this.colors.green
-                        } else {
-                            points.push([startX + gap, startY + (parentHeight / 2) + gap]);
-                            points.push([endX - gap, startY + (parentHeight / 2) + gap]);
-                            lineDirection.stroke = this.colors.lime //alter black
+                            points.push([startX + (parentWidth / 2) + gap, startY - (parentHeight / 2) - gap]);
+                            points.push([endX - (childWidth / 2) - gap, startY - (parentHeight / 2) - gap]);
                         }
                         break;
-                    default:
-
-                        if (startY - ((parentHeight + childHeight) / 2) - 2 * gap > endY) {
-                            points.push([startX + gap, startY - (parentHeight / 2) - gap]);
-                            points.push([endX - gap, startY - (parentHeight / 2) - gap]);
-
-                            lineDirection.stroke = this.colors.amber
-                        }
-                        else {
-                            points.push([startX + gap, endY - (childHeight / 2) - gap]);
-                            points.push([endX - gap, endY - (childHeight / 2) - gap]);
-                            lineDirection.stroke = this.colors.deepOrange
-                        }
-
+                    case this.rigth:
+                        lineDirection.stroke = this.colors.lime
+                        points.push([startX + (parentWidth / 2) + gap, endY]);
+                        break;
+                    case this.bottom:
+                        points.push([endX, startY + (parentHeight / 2) + gap]);
+                        lineDirection.stroke = this.colors.deepOrange
+                        isEndA = false;
+                        isStartA = false;
                         break;
                 }
-                //ret.stroke = 'gray'
 
                 if (isStartA) finalPoints = [...lineStartA, ...points];
                 else finalPoints = [...lineStartB, ...points];
+
                 if (isEndA) finalPoints = [...finalPoints, ...lineEndA];
                 else finalPoints = [...finalPoints, ...lineEndB];
-                
-                    
-                
+
+                //console.log(finalPoints)
+                lineDirection.start = finalPoints[0];
+
+                if(finalPoints.length > 2){
+                    for (let index = 0; index < finalPoints.length - 2; index++) {
+                        console.log(finalPoints[index])                      
+                    }
+                }
+
 
                 lineDirection.d = finalPoints.map((point, index) => {
                     return `${index === 0 ? 'M' : 'L'} ${point[0]} ${point[1]}`
@@ -263,7 +276,8 @@ const lineChart = {
     <g>
         <template v-for="line in lines">
             <path class="flowchart-line" :d="line.d" :stroke="line.stroke"/>
-            <path :transform="line.translate" d="M3 0 L8 0 A2 2 0 0 1 10 2 L10 7 A2 3 0 0 1 7.5 7.5 L2.5 2.5 A3 2 0 0 1 3 0" fill="GRAY" />            
+            <path :transform="line.translate" d="M3 0 L8 0 A2 2 0 0 1 10 2 L10 7 A2 3 0 0 1 7.5 7.5 L2.5 2.5 A3 2 0 0 1 3 0" fill="GRAY" />
+            <rect :y="line.start[1]-6" :x="line.start[0]-6" width="12" height="12" fill="gray" rx="2" ry="2"/>
         </template>
     </g>
   `
@@ -301,7 +315,7 @@ const svgChart = {
         })
     },
     mounted() {
-        
+
         this.cajaA = this.$refs.cajaA;
         this.cajaB = this.$refs.cajaB;
 
@@ -312,19 +326,19 @@ const svgChart = {
                 this.viewWidth = width;
             }
         });
-       
-        
+
+
         //this.$emit('update:modelValue', JSON.parse(JSON.stringify(this.modelValue)));
 
         resizeObserver.observe(this.$el);
     },
     methods: {
-        maxY(){
+        maxY() {
             return this.modelValue.reduce((acc, box) => {
                 return acc > box.y + box.height ? acc : box.y + box.height;
             }, 0);
         },
-        maxX(){
+        maxX() {
             return this.modelValue.reduce((acc, box) => {
                 return acc > box.x + box.width ? acc : box.x + box.width;
             }, 0);
@@ -372,11 +386,10 @@ const svgChart = {
 var app = createApp({
     data() {
         return {
-            boxs: [           
-                { x: 200, y: 200, width: 40, height: 40, id: 4 },     
+            boxs: [
                 { x: 4, y: 4, width: 96, height: 96, id: 1 },
-                { x: 20, y: 20, width: 180, height: 180, id:2 }
-            
+                { x: 20, y: 20, width: 180, height: 180, id: 2, parents: [1] },
+
             ]
         }
     },
@@ -401,3 +414,19 @@ app.component('box-chart', boxChart);
 app.component('line-chart', lineChart);
 
 app.mount('#app');
+
+
+function clasificarAngulo(Ax, Ay, Bx, By) {
+    const dx = Bx - Ax;
+    const dy = By - Ay;
+    const anguloRad = Math.atan2(dy, dx);
+    let anguloDeg = anguloRad * (180 / Math.PI);
+
+    anguloDeg = anguloDeg < 0 ? anguloDeg + 360 : anguloDeg;
+
+    if (anguloDeg > -45 && anguloDeg <= 45) return 0;
+    if (anguloDeg > 45 && anguloDeg <= 135) return 1;
+    if (anguloDeg > 135 && anguloDeg <= 225) return 2;
+    return 3;
+
+}
