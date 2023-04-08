148c628d338db964a7cef0b2903ee4030354893c
CoffeeDuck <davidlive0159@gmail.com>
2023-04-08 04:46:22 -0500

    disaster

--- script.js
+++ script.js
@@ -134,25 +134,57 @@ const lineChart = {
                 lime: '#CDDC39',
                 amber: '#FFC107',
                 deepOrange: '#FF5722'
-            }
+            },
+            toTop: 'Top',
+            toBottom: 'Bottom',
+            toLeft: 'Left',
+            toRigth: 'Rigth',
         }
     },
     mounted() {
 
     },
     methods: {
-        AnglePosition(Ax, Ay, Bx, By) {
-            const dx = Bx - Ax;
-            const dy = By - Ay;
+        angle(x1, y1, x2, y2) {
+            const dx = x2 - x1;
+            const dy = y2 - y1;
             const anguloRad = Math.atan2(dy, dx);
-            let anguloDeg = anguloRad * (180 / Math.PI);
+            let angle = anguloRad * (180 / Math.PI);
 
-            anguloDeg = anguloDeg < 0 ? anguloDeg + 360 : anguloDeg;
-            if (anguloDeg >= 315 || anguloDeg < 45) return 2;
-            if (anguloDeg >= 45 && anguloDeg < 135) return 3;
-            if (anguloDeg >= 135 && anguloDeg < 225) return 0;
+            angle = angle < 0 ? angle + 360 : angle;
+            return Math.round(angle);
+        },
+        AnglePosition(Ax, Ay, Bx, By) {
+            let angle = this.angle(Ax, Ay, Bx, By)
+            if (angle >= 315 || angle < 45) return 2;
+            if (angle >= 45 && angle < 135) return 3;
+            if (angle >= 135 && angle < 225) return 0;
             return 1;
-        }
+        },
+        AngleDirection(Ax, Ay, Bx, By) {
+            let angle = this.angle(Ax, Ay, Bx, By)
+            if (angle == 360 || angle == 0) return 'Rigth';
+            if (angle == 90) return 'Bottom';
+            if (angle == 180) return 'Left';
+            return 'Top';
+        },
+        horary(Ax, Ay, Bx, By) {
+            let angle = this.angle(Ax, Ay, Bx, By)
+            let isRotate = true;
+
+            let cuandrant = 3;
+            if (angle >= 0 && angle < 90) cuandrant = 0;
+            if (angle >= 90 && angle < 180) cuandrant = 1;
+            if (angle >= 180 && angle < 270) cuandrant = 2;
+
+            if (angle == 360 || angle == 0 || angle == 90 || angle == 180 || angle == 270) isRotate = false;
+
+            if (angle >= 0 && angle < 180) return { isRotate, isHorary: true, angle, cuandrant };
+            return { isRotate, isHorary: false, angle, cuandrant };
+        },
+        distance2Points(x1, y1, x2, y2) {
+            return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        },
     },
     computed: {
         posX() {
@@ -189,11 +221,6 @@ const lineChart = {
                 const lineStartB = [[startX, startY + (parentHeight / 2)], [startX, startY + (parentHeight / 2) + gap]];
                 const lineEndB = [[endX, endY - (childHeight / 2) - gap], [endX, endY - (childHeight / 2)]];
 
-                const childXIsMenor = (this.posX < parent.x) + 0;
-                const childYIsMenor = (this.posY < parent.y) + 0;
-
-                const state = parseInt(`${childXIsMenor}${childYIsMenor}`, 2);
-
                 const angle = this.AnglePosition(parent.x, parent.y, this.posX, this.posY);
                 let lineDirection = {
                     translate: `translate(${endX - (childWidth / 2) - 12}, ${endY - 5}) rotate(45, 5, 3.5)`
@@ -206,21 +233,19 @@ const lineChart = {
                     case this.left:
                         lineDirection.stroke = this.colors.indigo
                         isEndA = false;
-                        
+
                         if (endY - (childHeight / 2) < startY - (parentHeight / 2)) {
                             points.push([startX + (parentWidth / 2) + gap, endY - (childHeight / 2) - gap]);
                         }
-                        else{
-                                
-                            
+                        else {
                             isStartA = false;
-                            points.push([startX-(parentWidth / 2) - gap, startY + (parentHeight / 2) + gap]);
-                            
+                            points.push([startX - (parentWidth / 2) - gap, startY + (parentHeight / 2) + gap]);
+
                             if (endY - (childHeight / 2) - gap * 2 < startY + (parentHeight / 2)) {
-                                points.push([startX -(parentWidth / 2) - gap, endY - (childHeight / 2) - gap]);
+                                points.push([startX - (parentWidth / 2) - gap, endY - (childHeight / 2) - gap]);
                             }
                             else {
-                                points.push([endX,startY + (parentHeight / 2) + gap]);
+                                points.push([endX, startY + (parentHeight / 2) + gap]);
                             }
                         }
 
@@ -255,12 +280,82 @@ const lineChart = {
 
                 //console.log(finalPoints)
                 lineDirection.start = finalPoints[0];
+                const gapCorner = 16;
+                let pointsWidthoutCorner = [finalPoints[0]]
+                if (finalPoints.length > 2) {
+                    for (let index = 1; index < finalPoints.length - 1; index++) {
+                        const pA = finalPoints[index - 1]
+                        const pB = finalPoints[index]
+                        const pC = finalPoints[index + 1]
 
-                if(finalPoints.length > 2){
-                    for (let index = 0; index < finalPoints.length - 2; index++) {
-                        console.log(finalPoints[index])                      
+                        const distance1 = this.distance2Points(pA[0], pA[1], pB[0], pB[1])
+                        const distance2 = this.distance2Points(pB[0], pB[1], pC[0], pC[1])
+
+
+                        const minDistance = Math.min(distance1 / 2, distance2 / 2)
+                        const customGap = minDistance < gapCorner ? minDistance : gapCorner
+
+
+                        const angle1 = this.AngleDirection(pA[0], pA[1], pB[0], pB[1]);
+                        const angle2 = this.AngleDirection(pB[0], pB[1], pC[0], pC[1]);
+
+                        const horary = this.horary(pA[0], pA[1], pC[0], pC[1])
+
+                        if (horary.isRotate) {
+                            console.log(horary)
+
+                            if (angle1 == this.toTop) pointsWidthoutCorner.push([pB[0], pB[1] + customGap])
+                            if (angle1 == this.toBottom) pointsWidthoutCorner.push([pB[0], pB[1] - customGap])
+                            if (angle1 == this.toLeft) pointsWidthoutCorner.push([pB[0] + customGap, pB[1]])
+                            if (angle1 == this.toRigth) pointsWidthoutCorner.push([pB[0] - customGap, pB[1]])
+
+                            let cornet = { r: customGap, isHorary: true }
+
+                            if (angle2 == this.toTop) {
+                                cornet.x = pB[0]
+                                cornet.y = pB[1] - customGap
+                            }
+                            if (angle2 == this.toBottom) {
+                                cornet.x = pB[0]
+                                cornet.y = pB[1] + customGap
+                            }
+                            if (angle2 == this.toLeft) {
+                                cornet.x = pB[0] - customGap
+                                cornet.y = pB[1]
+                            }
+                            if (angle2 == this.toRigth) {
+                                cornet.x = pB[0] + customGap
+                                cornet.y = pB[1]
+                            }
+
+                            console.log(angle1, angle2)
+                            if (
+                                angle1 == this.toRigth && angle2 == this.toTop ||
+                                angle1 == this.toTop && angle2 == this.toLeft ||
+                                angle1 == this.toLeft && angle2 == this.toBottom ||
+                                angle1 == this.toBottom && angle2 == this.toRigth
+                            ) {
+                                cornet.isHorary = false
+                            }
+
+                            pointsWidthoutCorner.push(cornet)
+
+
+
+                        } else {
+                            pointsWidthoutCorner.push(pB)
+                        }
+
+
                     }
                 }
+                pointsWidthoutCorner.push(finalPoints[finalPoints.length - 1])
+
+                lineDirection.d2 = pointsWidthoutCorner.map((point, index) => {
+                    if (Array.isArray(point)) return `${index === 0 ? 'M' : 'L'} ${point[0]} ${point[1]}`
+                    console.log(point)
+                    return `A ${point.r} ${point.r} 0 0 ${point.isHorary ? 1 : 0} ${point.x} ${point.y}`
+                }).join(' ');
 
 
                 lineDirection.d = finalPoints.map((point, index) => {
@@ -275,7 +370,7 @@ const lineChart = {
     template: `
     <g>
         <template v-for="line in lines">
-            <path class="flowchart-line" :d="line.d" :stroke="line.stroke"/>
+            <path class="flowchart-line" :d="line.d2" stroke="purple"/>
             <path :transform="line.translate" d="M3 0 L8 0 A2 2 0 0 1 10 2 L10 7 A2 3 0 0 1 7.5 7.5 L2.5 2.5 A3 2 0 0 1 3 0" fill="GRAY" />
             <rect :y="line.start[1]-6" :x="line.start[0]-6" width="12" height="12" fill="gray" rx="2" ry="2"/>
         </template>
@@ -420,13 +515,13 @@ function clasificarAngulo(Ax, Ay, Bx, By) {
     const dx = Bx - Ax;
     const dy = By - Ay;
     const anguloRad = Math.atan2(dy, dx);
-    let anguloDeg = anguloRad * (180 / Math.PI);
+    let angle = anguloRad * (180 / Math.PI);
 
-    anguloDeg = anguloDeg < 0 ? anguloDeg + 360 : anguloDeg;
+    angle = angle < 0 ? angle + 360 : angle;
 
-    if (anguloDeg > -45 && anguloDeg <= 45) return 0;
-    if (anguloDeg > 45 && anguloDeg <= 135) return 1;
-    if (anguloDeg > 135 && anguloDeg <= 225) return 2;
+    if (angle > -45 && angle <= 45) return 0;
+    if (angle > 45 && angle <= 135) return 1;
+    if (angle > 135 && angle <= 225) return 2;
     return 3;
 
 }
